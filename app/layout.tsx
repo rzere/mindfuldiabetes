d9ca068f529a3cea1e7ca65e4093a88c827d6097
rzere <rzere@rollins.edu>
2024-04-03 22:59:17 +0300

    hotfix

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default async function RootLayout({
       <body className={cx(sfPro.variable, inter.variable)}>
         <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
         <Suspense fallback="...">
+          <Nav />
         </Suspense>
         <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
           {children}
