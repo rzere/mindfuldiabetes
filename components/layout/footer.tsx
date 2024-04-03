74b7dad04443264a85eb414ced67d8a4323e6a4f
rzere <rzere@rollins.edu>
2024-04-03 23:23:41 +0300

    changed steven's name

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -7,22 +7,13 @@ export default function Footer() {
         A project by{" "}
         <a
           className="font-semibold text-gray-600 underline-offset-4 transition-colors hover:underline"
-          href="https://twitter.com/steventey"
+          href="https://twitter.com/ruzventure"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Steven Tey
+          @ruzventure
         </a>
       </p>
-      <a
-        href="https://www.buymeacoffee.com/steventey"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="mx-auto mt-2 flex max-w-fit items-center justify-center space-x-2 rounded-lg border border-gray-200 bg-white px-6 py-2 transition-all duration-75 hover:scale-105"
-      >
-        <BuyMeACoffee className="h-6 w-6" />
-        <p className="font-medium text-gray-600">Buy me a coffee</p>
-      </a>
     </div>
   );
 }
