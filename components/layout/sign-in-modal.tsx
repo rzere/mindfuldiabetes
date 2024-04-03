03d26cdb818786104c722704468b8f837ff456a7
rzere <rzere@rollins.edu>
2024-04-03 23:09:31 +0300

    commit

--- components/layout/sign-in-modal.tsx
+++ components/layout/sign-in-modal.tsx
@@ -27,15 +27,14 @@ const SignInModal = ({
             <Image
               src="/logo.png"
               alt="Logo"
-              className="h-10 w-10 rounded-full"
-              width={20}
-              height={20}
+              className="h-10 w-10"
+              width={30}
+              height={30}
             />
           </a>
           <h3 className="font-display text-2xl font-bold">Sign In</h3>
           <p className="text-sm text-gray-500">
-            This is strictly for demo purposes - only your email and profile
-            picture will be stored.
+            Please log-in with your Google Account to view your leads.
           </p>
         </div>
 
