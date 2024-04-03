7879a35333450a6621f0d6f7d7f5e368c31003ed
rzere <rzere@rollins.edu>
2024-04-03 22:37:18 +0300

    commit

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -2,7 +2,7 @@
 import { ImageResponse } from "next/server";
 
 export const runtime = "edge";
-export const alt = "Precedent - Building blocks for your Next.js project";
+export const alt = "LeadShark - Generate Leads like Magic";
 export const contentType = "image/png";
 
 export default async function OG() {
