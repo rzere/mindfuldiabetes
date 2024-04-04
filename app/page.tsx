593d0d2a335fad7c6238580fad091b0fc41b3e97
rzere <rzere@rollins.edu>
2024-04-04 15:06:48 +0300

    twitter link change

--- app/page.tsx
+++ app/page.tsx
@@ -27,14 +27,13 @@ export default async function Home() {
     <>
       <div className="z-10 w-full max-w-xl px-5 xl:px-0">
         <a
-          href="https://twitter.com/steventey/status/1613928948915920896"
+          href="https://twitter.com/leadshark-io"
           target="_blank"
           rel="noreferrer"
           className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"
         >
-          <Twitter className="h-5 w-5 text-[#1d9bf0]" />
           <p className="text-sm font-semibold text-[#1d9bf0]">
-            Introducing Precedent
+            Introducing LeadShark
           </p>
         </a>
         <h1
