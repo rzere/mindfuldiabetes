9482ae0a7078405cee8c11f731896e20d1d0b8b6
rzere <rzere@rollins.edu>
2024-04-04 12:54:52 +0300

    button change

--- app/page.tsx
+++ app/page.tsx
@@ -65,15 +65,8 @@ export default async function Home() {
               fill="currentColor"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <path
-                d="M12 4L20 20H4L12 4Z"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
             </svg>
-            <p>Deploy to Vercel</p>
+            <p>Get Browser Extension</p>
           </a>
           <a
             className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
