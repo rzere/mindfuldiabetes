6fa0008ae1cce600d880b46a7005467c4b931522
rzere <rzere@rollins.edu>
2024-04-04 12:35:36 +0300

    image fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -9,8 +9,16 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+          scope: "openid profile email https://www.googleapis.com/auth/userinfo.profile" // Include the profile image scope
+        }
+      }
+    })
+  ] 
 };
 
 const handler = NextAuth(authOptions);
