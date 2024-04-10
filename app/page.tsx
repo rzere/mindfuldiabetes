7e30890a635c889f2a52b0be31ad2fe47d3db275
rzere <rzere@rollins.edu>
2024-04-10 21:04:35 +0300

    github link change

--- app/page.tsx
+++ app/page.tsx
@@ -69,7 +69,7 @@ export default async function Home() {
           </a>
           <a
             className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
-            href="https://mindfuldiabetes.org"
+            href="https://github.com/rzere/mindfuldiabetes"
             target="_blank"
             rel="noopener noreferrer"
           >
