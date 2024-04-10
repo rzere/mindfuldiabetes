621f495a4038f2d554e7925e7de52ae0e773a70d
rzere <rzere@rollins.edu>
2024-04-10 20:50:46 +0300

    hero text

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ export default async function Home() {
           className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
         >
-          Generate Leads like Magic
+          Ask me about Type III Diabetes
         </h1>
         <p
           className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
