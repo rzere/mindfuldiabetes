12bbf0beff61f5462e2d53c1fcec2fb57c048f1f
rzere <rzere@rollins.edu>
2024-04-15 14:22:41 +0300

    navbar text change

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -30,7 +30,7 @@ export default function NavBar({ session }: { session: Session | null }) {
               height="30"
               className="mr-2 rounded-sm"
             ></Image>
-            <p>Mindful Diabetes AI</p>
+            <p>Mindful Diabetes Inc.</p>
           </Link>
           <div>
             {session ? (
