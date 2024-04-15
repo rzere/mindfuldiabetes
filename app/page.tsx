535a29d3b6e30dbb419835ced225d14bab22c75d
rzere <rzere@rollins.edu>
2024-04-15 17:53:46 +0300

    revert

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home(){
     return(
       <>
       <div className="z-10 w-full max-w-xl px-5 xl:px-0">
-      <Chatbot />
+      {session && <Chatbot />}
       </div>
       <div className="z-10 w-full max-w-xl px-5 xl:px-0">
         <a
