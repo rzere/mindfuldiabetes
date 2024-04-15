12691c50c964c784257fea5b8d0c4c2cd5861ce8
rzere <rzere@rollins.edu>
2024-04-15 14:04:47 +0300

    text changes

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default async function Home() {
           className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
           style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
         >
-          An 
+          Navigate your health with our mdAI Assistant 
         </p>
         <div
           className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0"
