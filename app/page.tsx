f65f6b76f210de9544a1c24984346ba4e177a1a4
rzere <rzere@rollins.edu>
2024-04-16 18:29:53 +0300

    JERRI image added

--- app/page.tsx
+++ app/page.tsx
@@ -18,13 +18,13 @@ export default function Home(){
           className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
         >
-          Mindful Diabetes AI Assistant
+          Meet JERRI: Mindful Diabetes AI Assistant
         </h1>
         <p
           className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
           style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
         >
-          Navigate your health with our mdAI Assistant 
+          Navigate your health with JERRI 🥕
         </p>
         <div
           className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0"
