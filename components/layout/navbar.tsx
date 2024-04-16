4a465a9b15fce5cf883c93b1dac99298e925f79a
rzere <rzere@rollins.edu>
2024-04-16 15:28:02 +0300

    navbar text change

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -7,11 +7,49 @@ import { useSignInModal } from "./sign-in-modal";
 import UserDropdown from "./user-dropdown";
 //import { Session } from "next-auth";
 //import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import {useKindeBrowserClient} from "@kinde-oss/kinde-auth-nextjs";
 
 export default function NavBar() {
   const { SignInModal, setShowSignInModal } = useSignInModal();
   const scrolled = useScroll(50);
-
+  const {
+    user
+} = useKindeBrowserClient();
+if (user != null) return (
+  <>
+      <SignInModal />
+      <div
+        className={`fixed top-0 w-full flex justify-center ${
+          scrolled
+            ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
+            : "bg-white/0"
+        } z-30 transition-all`}
+      >
+        <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
+          <Link href="/" className="flex items-center font-display text-2xl">
+            <Image
+              src="/logo.png"
+              alt="MD logo"
+              width="30"
+              height="30"
+              className="mr-2 rounded-sm"
+            ></Image>
+          </Link>
+          <div>
+          <button
+                 className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
+                 onClick={() => setShowSignInModal(true)}
+               >
+                 Logout
+               </button>
+          {/* <LoginLink>Sign in</LoginLink>
+          <RegisterLink>Sign up</RegisterLink> */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+)
   return (
     <>
       <SignInModal />
