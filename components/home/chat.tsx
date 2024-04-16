6edeac28e0e2874affdb7b86888bd7185ebc6a1f
rzere <rzere@rollins.edu>
2024-04-16 13:43:03 +0300

    fix

--- components/home/chat.tsx
+++ components/home/chat.tsx
@@ -1,8 +1,10 @@
 import Chatbot from "./Chatbot";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+//import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default async function Nav() {
-  const session = await getServerSession(authOptions);
+export default async function Chat() {
+  //const session = await getServerSession(authOptions);
+  const session = await getServerSession(handleAuth);
   return <Chatbot session={session} />;
 }
