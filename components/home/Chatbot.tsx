2afe50e27e5fb48c23d1c27749fe2da0491ac361
rzere <rzere@rollins.edu>
2024-04-16 18:31:51 +0300

    JERRI image changed

--- components/home/Chatbot.tsx
+++ components/home/Chatbot.tsx
@@ -8,7 +8,7 @@ import {useKindeBrowserClient} from "@kinde-oss/kinde-auth-nextjs";
 
 //export default function Chatbot({ session }: { session: Session | null }) {
 export default function Chatbot() {
-const jerriAvatarUrl = "https://scontent.cdninstagram.com/v/t51.2885-15/60228238_434219193821264_7385769686695668070_n.jpg?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi45NjJ4OTYyLnNkci5mMjg4NSJ9&_nc_ht=scontent.cdninstagram.com&_nc_cat=108&_nc_ohc=BNGsdFe5YjsAb6rmeTI&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MjA0Njc5NjU5NzYzNTczNDIyMg%3D%3D.2-ccb7-5&oh=00_AfD2_sMJd4TalbiJIiZ6SSLY9fs7ObmSkVLzrvs6xVAWDg&oe=66225A01&_nc_sid=10d13b";
+const jerriAvatarUrl = "https://raw.githubusercontent.com/rzere/mindfuldiabetes/main/public/jerri.jpeg";
 const { messages, input, handleInputChange, handleSubmit } = useChat();
 //const { email, name } = session?.user || {};
 const {
