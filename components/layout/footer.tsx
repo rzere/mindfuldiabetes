195c728e0c9c0bf62bd91d2d4fc5fc1b0c35aaf6
rzere <rzere@rollins.edu>
2024-04-29 19:25:47 +0300

    footer change

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -4,14 +4,14 @@ export default function Footer() {
   return (
     <div className="absolute w-full py-5 text-center">
       <p className="text-gray-500">
-        A project by{" "}
+        View our {" "}
         <a
           className="font-semibold text-gray-600 underline-offset-4 transition-colors hover:underline"
-          href="https://stoic.ist"
+          href="https://us.umami.is/share/im7OHDz0Lwkhjb2c"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Stoic.
+          Live Analytics.
         </a>
       </p>
     </div>
