88f3d87107023f6e96575f618a12c080cafc2cd7
rzere <rzere@rollins.edu>
2024-04-30 23:15:52 +0300

    SEO fixed (sitemap & robots.txt)

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -11,11 +11,11 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   return [
     {
-      url: "https://precedent.dev",
+      url: "https://www.mindfuldiabetes.ai",
       lastModified: new Date(),
     },
     ...users.map((user) => ({
-      url: `https://precedent.dev/${user.id}`,
+      url: `https://www.mindfuldiabetes.ai/${user.id}`,
       lastModified: new Date(),
     })),
   ];
