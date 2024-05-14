f41cbca4992f6537f195da1dd128eb5663b299c6
rzere <rzere@rollins.edu>
2024-05-14 16:30:59 +0300

    adjusted button roundness

--- components/home/Chatbot.tsx
+++ components/home/Chatbot.tsx
@@ -73,25 +73,25 @@ export default function Chatbot() {
               <div className="grid grid-cols-2 gap-4 mb-4">
                 <button 
                   onClick={(e) => handlePresetQuestion(e, "What is type 3 diabetes?")} 
-                  className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200 text-sm font-semibold text-[#1d9bf0]"
+                  className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-lg bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200 text-sm font-semibold text-[#1d9bf0]"
                 >
                   What is type 3 diabetes?
                 </button>
                 <button 
                   onClick={(e) => handlePresetQuestion(e, "What is insulin resistance in the brain?")} 
-                  className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200 text-sm font-semibold text-[#1d9bf0]"
+                  className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-lg bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200 text-sm font-semibold text-[#1d9bf0]"
                 >
                   What is insulin resistance in the brain?
                 </button>
                 <button 
                   onClick={(e) => handlePresetQuestion(e, "What is the connection between Alzheimers Disease & Type 2 Diabetes?")} 
-                  className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200 text-sm font-semibold text-[#1d9bf0]"
+                  className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-lg bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200 text-sm font-semibold text-[#1d9bf0]"
                 >
                   What is the connection between Alzheimers Disease & Type 2 Diabetes?
                 </button>
                 <button 
                   onClick={(e) => handlePresetQuestion(e, "Can you make me a 21 day walking exercise routine?")} 
-                  className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200 text-sm font-semibold text-[#1d9bf0]"
+                  className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-lg bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200 text-sm font-semibold text-[#1d9bf0]"
                 >
                   Can you make me a 21 day walking exercise routine?
                 </button>
@@ -102,7 +102,7 @@ export default function Chatbot() {
             <input
               className="container rounded-full mx-auto px-4 py-8 flex-grow flex items-center justify-center text-gray-500"
               value={input}
-              placeholder="Press enter to submit your query!"
+              placeholder="Press enter to submit your query"
               onChange={handleInputChange}
             />
           </form>
